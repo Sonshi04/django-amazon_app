f0326ccdab7839402d536b0de6150a2392994669
Sonshi04 <arousalofneetysk6@gmail.com>
2022-09-06 08:00:04 +0900

    仮完成2

--- amazon_crawler/settings.py
+++ amazon_crawler/settings.py
@@ -131,6 +131,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+#!!!本番環境は消すかも!!!
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- crawler_main/urls.py
+++ crawler_main/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from .views import HomeView, AboutView, crawl
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
-  path('result/', crawl, name='result'),
   path('',HomeView.as_view(), name='home'),
-  path('about/',AboutView.as_view(), name='about'),
+  path('result/', crawl, name='result'),
+  path('about/',AboutView.as_view(),name='about'),
 ]
